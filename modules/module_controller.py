74fccd1df64f073e05aa5d722e3c9ee8e1b337af
adjr01 <sthep90@gmail.com>
2025-12-03 00:18:59 +0600

    setting chroma

--- modules/module_controller.py
+++ modules/module_controller.py
@@ -1,6 +1,6 @@
 from modules.Parsers.text_parser import TextParser
-from langchain_community.vectorstores import chroma
-
+from langchain_community.vectorstores import Chroma
+from langchain_ollama.embeddings import OllamaEmbeddings
 
 
 
@@ -13,6 +13,20 @@ def load_storage():
     text_parser.load_from(storage_dir,True)
     return text_parser.process()
 
+def get_embeddings():
+    return OllamaEmbeddings(model='qwen3-embedding:0.6b')
+
+def get_vector_store(documents,embeddings,collection_name,persist_dir):
+    return Chroma.from_documents(
+        documents=documents,
+        embedding=embeddings,
+        collection_name=collection_name,
+        persist_dir=persist_dir,
+    )
+
 def main():
-    load_storage()
+    docs = load_storage()
+    vector_store = get_vector_store(documents=docs,embeddings=get_embeddings(),collection_name=collection_name,persist_dir=chroma_persist)
+
+
 
