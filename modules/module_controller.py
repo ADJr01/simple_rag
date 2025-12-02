2ee2ffd752296410df281398743a51624a67d427
adjr01 <sthep90@gmail.com>
2025-12-02 23:09:33 +0600

    working on text parsing

--- modules/module_controller.py
+++ modules/module_controller.py
@@ -1,10 +1,15 @@
 from modules.Parsers.text_parser import TextParser
 from langchain_community.vectorstores import chroma
+
+
+
+
 storage_dir = '/home/ad/Desktop/Project/simple_rag/modules/storage/'
+chroma_persist = '/home/ad/Desktop/Project/simple_rag/modules/db/'
 collection_name = 'rag_memory'
 
 def main():
     text_parser = TextParser()
     text_parser.load_from(storage_dir,True)
     t = text_parser.process()
-    print(t)
+
