ce4ec5587ba5d21a804ca1e64c98721ce21238c2
Rahat Adnan <sthep90@gmail.com>
2025-12-05 10:56:03 +0600

    simple rag complete

--- modules/module_controller.py
+++ modules/module_controller.py
@@ -1,12 +1,34 @@
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+
 from modules.Parsers.text_parser import TextParser
 from langchain_community.vectorstores import Chroma
 from langchain_ollama.embeddings import OllamaEmbeddings
 
 
 
-storage_dir = '/home/ad/Desktop/Project/simple_rag/modules/storage/'
-chroma_persist = '/home/ad/Desktop/Project/simple_rag/modules/db/'
+storage_dir = 'D:\\Projects\\Personal\\LLM\\simple_rag\\modules\\storage'
+chroma_persist = 'D:\\Projects\\Personal\\LLM\\simple_rag\\modules\\db'
 collection_name = 'rag_memory'
+sys_prompt = '''
+    You're an helpful assistant for question answering task.
+    use the following pieces of retrieved context to answer the question:
+
+    if you don't know the answer then simply reply You Can't help with that question.
+
+    if users query is complex then Break the
+    query and divide them into logical sub-queries 
+    then solve the problem step by step.
+
+    Context: {context}
+'''
+
+prompt = ChatPromptTemplate.from_messages([
+    {"role": "system", "content": sys_prompt},
+    {"role": "human", "content": "{input}"}
+])
 
 def load_storage():
     text_parser = TextParser()
@@ -21,7 +43,14 @@ def get_vector_store(documents,embeddings,collection_name,persist_dir):
         documents=documents,
         embedding=embeddings,
         collection_name=collection_name,
-        persist_dir=persist_dir,
+        persist_directory=persist_dir,
+    )
+
+
+def create_llm():
+    return ChatOllama(
+        model='deepseek-r1:8b',
+        reasoning=True
     )
 
 
@@ -33,5 +62,14 @@ def main():
     retrival = vector_store.as_retriever(
         search_kwargs={"k":2},
     )
+    llm = create_llm()
+    document_chain = create_stuff_documents_chain(llm=llm,prompt=prompt)
+    rag_chain = create_retrieval_chain(retrival,document_chain)
+    str=str=input("Enter your question: ")
+    while str.lower().strip() != 'q' or str.lower().strip() != 'exit':
+        str = str.strip()
+        response = rag_chain.invoke({"input":str})
+        print(response["answer"])
+        str = input("Enter your question: ")
 
 
